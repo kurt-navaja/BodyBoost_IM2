dd0752a89b0e7225d767227907103cd4980a580a
asaphb <kim.bacaltos@cit.edu>
2024-10-02 10:38:42 +0800

    settings adding urls

--- BodyBoost/settings.py
+++ BodyBoost/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Signup",
+    "Login",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
