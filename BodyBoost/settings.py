c6326042a8d8c66c674aafdc4d0a527786c429cb
kimasaph <kim.bacaltos@cit.edu>
2024-12-03 14:04:33 +0800

    pp update
    better working profile picture now

--- BodyBoost/settings.py
+++ BodyBoost/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'NotifAll' / 'static',  # Add this line
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
